42119dcc4ec29db54d01875e53a2cf9aaae77eb4
池田将臣 <maple.consult00@gmail.com>
2022-03-09 03:53:20 +0900

    index,show,newページのUI作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,37 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show]
     def index
-        @articles = Article.all
+        # @articles = Article.all
     end
 
     def show
-        @articles = Article.find(params[:id])
+    end
+
+    def new
+        @article = current_user.articles.build
+    end
+    
+    def create
+        @article = current_user.articles.build(article_params)
+        if @article.save
+          redirect_to article_path(@article), notice: '保存しました'
+        else
+          flash.now[:error] = '保存できません'
+          render :new
+        end
+    end
+    
+
+      
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
+
+
+    def set_article
+        @article = Article.find(params[:id])
     end
 
 end
