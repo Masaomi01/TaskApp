063a1f5ab6910497e24fe3a4d8676bcdd61dd883
池田将臣 <maple.consult00@gmail.com>
2022-03-10 17:39:25 +0900

    index,show,newのUI修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show]
     def index
-        # @articles = Article.all
+        @articles = Article.all
     end
 
     def show
