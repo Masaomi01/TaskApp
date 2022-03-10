7d9c7f9709e3dbbcb3fc46dd367cf0bb84c2ff91
池田将臣 <maple.consult00@gmail.com>
2022-03-11 05:03:20 +0900

    編集、消去リンクの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
@@ -20,6 +22,28 @@ class ArticlesController < ApplicationController
           render :new
         end
     end
+
+    def edit
+        @article = current_user.articles.find(params[:id])
+    end
+    
+    
+    def update
+        @article = current_user.articles.find(params[:id])
+        if @article.update(article_params)
+          redirect_to article_path(@article), notice: '更新しました'
+        else
+          flash.now[:error] = '更新できませんでした'
+          render :edit
+        end
+    end
+    
+    def destroy
+        article = current_user.articles.find(params[:id])
+        article.destroy!
+        redirect_to root_path, notice: '削除に成功しました'
+    end
+    
     
 
       
