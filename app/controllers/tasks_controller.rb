c08da188de5e5ace9838953519768f018e06d68e
池田将臣 <maple.consult00@gmail.com>
2022-03-17 18:59:30 +0900

    task詳細ページの作成 eyecatchの機能の導入

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,10 @@
 class TasksController < ApplicationController
+
+
+    def show
+        @board = Article.find(params[:article_id])
+        @task = @board.tasks.find(params[:id])
+    end
     
     
     def new
@@ -19,7 +25,7 @@ class TasksController < ApplicationController
 
     private
     def task_params
-        params.require(:task).permit(:content, :title, :deadline).merge(user_id: current_user.id)
+        params.require(:task).permit(:content, :title, :deadline, :eyecatch).merge(user_id: current_user.id)
     end
     
 end 
