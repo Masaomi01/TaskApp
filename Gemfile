7477e8c8dfff4189a688ec752b8c3f0995d202ab
池田将臣 <maple.consult00@gmail.com>
2022-03-29 14:02:09 +0900

    編集、消去機能の実装。 編集ページの追加

--- Gemfile
+++ Gemfile
@@ -47,7 +47,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'erb2haml'
-  gem 'pry-rails'
+  gem 'pry-byebug'
 end
 
 group :test do
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,9 @@ class TasksController < ApplicationController
 
     def show
         @article = Article.find(params[:article_id])
-        @task = @article.tasks.find_by(params[:task_id])
+        @task = @article.tasks.find(params[:id])
         
     end
-    
     
     def new
         article = Article.find(params[:article_id])
@@ -23,6 +22,31 @@ class TasksController < ApplicationController
           render :new
         end
     end
+    def edit
+        @task = current_user.tasks.find(params[:id])
+        
+
+    end
+    
+    def update
+        @task = current_user.tasks.find(params[:id])
+        
+        if @task.update(task_params)
+          redirect_to article_task_path(@task.article), notice: '更新しました'
+        else
+          flash.now[:error] = '更新できませんでした'
+          render :edit
+        end
+    end
+
+   
+    def destroy
+        article = Article.find(params[:article_id])
+        task = article.tasks.find_by(user_id: current_user.id)
+        task.destroy!
+        redirect_to article_path(article), notice: '削除に成功しました'
+    end
+    
 
     private
     def task_params
